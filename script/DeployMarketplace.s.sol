7d785257352306033ef16438688b9d4cfdba2dab
Philippe Schommers <philippe@schommers.be>
2022-12-20 23:40:02 +0100

    build: update marketplace deploy script

--- script/DeployMarketplace.s.sol
+++ script/DeployMarketplace.s.sol
@@ -10,6 +10,7 @@ import { RolesAuthority } from 'solmate/auth/authorities/RolesAuthority.sol';
 import { AuthorityUtils } from './lib/AuthorityUtils.sol';
 
 // Contracts
+import { BonusChests } from 'src/BonusChests.sol';
 import { Characters } from 'src/Characters.sol';
 import { Chests } from 'src/Chests.sol';
 import { Marketplace, Types } from 'src/Marketplace.sol';
@@ -21,26 +22,16 @@ contract DeployMarketplaceScript is Script, AuthorityUtils {
 	uint256 fee = 500;
 
 	// Testnet
+	/*
 	ERC20 currency = ERC20(0xa9BeF92eD63C997b418A86E0E14a4fE79e639f5A);
-
-	Characters characters =
-		Characters(0x91aF80F5AC26727D97272F6244143439F3C2C2aa);
-	Chests chests = Chests(0x4874a452c19eE75dAADf0E740de8643821cb3D7d);
-	Stones stones = Stones(0x802FAf1A3a7dE4d8BebB0433AE4009a501CDb531);
 	RolesAuthority rolesAuthority =
 		RolesAuthority(0x37D98eDb9C93c8E70ec737E9c51d16C908fBb33b);
+	*/
 
 	// Production
-	/*
 	ERC20 currency = ERC20(0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56);
-
-	Characters characters =
-		Characters(0xfdeF2aB73198C196CBceBD0c33A77CB1CD907729);
-	Chests chests = Chests(0x26Fdd867f09B26440259f550ecb36D07A0cD954F);
-	Stones stones = Stones(0x20d9E48C39AeE6F21281827CFeE76eBa3366097d);
 	RolesAuthority rolesAuthority =
 		RolesAuthority(0x7b1fd50a4a046858575a0794a5d05Ae4170469a9);
-	*/
 
 	function setUp() public {
 		authority = rolesAuthority;
@@ -56,9 +47,32 @@ contract DeployMarketplaceScript is Script, AuthorityUtils {
 		setupMarketplace(marketplace);
 
 		// Allow tokens
-		marketplace.allowToken(address(characters), Types.ERC721);
-		marketplace.allowToken(address(chests), Types.ERC1155);
-		marketplace.allowToken(address(stones), Types.ERC20);
+		// Production
+		// Characters
+		marketplace.allowToken(
+			0xfdeF2aB73198C196CBceBD0c33A77CB1CD907729,
+			Types.ERC721
+		);
+		marketplace.allowToken(
+			0x4a2E149C0e37D8970c85cDA99a3DeFfe70999CAF,
+			Types.ERC721
+		);
+
+		// Stones
+		marketplace.allowToken(
+			0x20d9E48C39AeE6F21281827CFeE76eBa3366097d,
+			Types.ERC20
+		);
+
+		// Chests
+		marketplace.allowToken(
+			0xFe6af84fd3415f94FBB41Dac8EA558a758Ea138f,
+			Types.ERC1155
+		);
+		marketplace.allowToken(
+			0x26Fdd867f09B26440259f550ecb36D07A0cD954F,
+			Types.ERC721
+		);
 
 		vm.stopBroadcast();
 	}
